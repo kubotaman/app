9aad6556bed88072e32d8cf7f3e3fd2f2847e829
kubotaman <kubota@ups.ne.jp>
2013-10-01 16:29:54 +0900

    コミット

--- classes/controller/welcome.php
+++ classes/controller/welcome.php
@@ -23,8 +23,8 @@ class Controller_Welcome extends Controller
             $data['test'] = Test::get_results();
             
             //$view = array('hoge' => 'piyo');
-            //return Response::forge(View_Smarty::forge('welcome/index',$data));
-            return Response::forge(View::forge('welcome/index',$data));
+            return Response::forge(View_Smarty::forge('welcome/index',$data));
+            //return Response::forge(View::forge('welcome/index',$data));
 	}
 
 	/**
