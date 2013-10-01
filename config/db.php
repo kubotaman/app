52d7bba211b7241bde2c9258a3b2e50de055ca38
kubotaman <kubota@ups.ne.jp>
2013-10-01 14:59:25 +0900

    コミット

--- config/db.php
+++ config/db.php
@@ -6,28 +6,42 @@
  */
 
 return array(
-	'active' => 'default',
+        'active' => 'testdb',
 
-	/**
-	 * Base config, just need to set the DSN, username and password in env. config.
-	 */
-	'default' => array(
-		'type'        => 'pdo',
-		'connection'  => array(
-			'persistent' => false,
-		),
-		'identifier'   => '`',
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'enable_cache' => true,
-		'profiling'    => false,
-	),
+        /**
+         * Base config, just need to set the DSN, username and password in env. config.
+         */
+        'default' => array(
+                'type'        => 'pdo',
+                'connection'  => array(
+                        'persistent' => false,
+                ),
+                'identifier'   => '`',
+                'table_prefix' => '',
+                'charset'      => 'utf8',
+                'enable_cache' => true,
+                'profiling'    => false,
+        ),
 
-	'redis' => array(
-		'default' => array(
-			'hostname'  => '127.0.0.1',
-			'port'      => 6379,
-		)
-	),
+        'redis' => array(
+                'default' => array(
+                        'hostname'  => '127.0.0.1',
+                        'port'      => 6379,
+                )
+        ),
+        'testdb' => array(
+            'type'   => 'mysqli',
+            'connection' => array(
+                'hostname'   => 'localhost',
+                'database'   => 'cas8rwl_aip',
+                'username'   => 'cas8rwl_aip',
+                'password'   => '7Th4udXS',
+                'persistent' => FALSE,
+            ),
+            'table_prefix' => '',
+            'charset'      => 'utf8',
+            'caching'      => false,
+            'profiling'    => true,
+        ),
 
 );
